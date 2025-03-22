f2d205ff22c1eaf390baa1093b1e6bad7ff3818e
Omkar Khoche <omkarkhoche3448@gmail.com>
2025-03-22 13:19:36 +0530

    Added React Router  and set up Redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import JobDetailsPage from "./pages/JobDetailsPage";
+
 function App() {
   return (
     <>
-      <div>Blue Bit</div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/job" element={<JobDetailsPage/>}/>
+      </Routes>
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducer";
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
