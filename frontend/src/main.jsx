068785bbb69e7ce50155bbe4a42de4b0f21335dd
Omkar Khoche <omkarkhoche3448@gmail.com>
2025-03-29 09:17:21 +0530

    feat: integrate Clerk authentication provider in main application
    This commit adds the `ClerkProvider` to the main application component, enabling user authentication features. It also includes a check for the presence of the Clerk publishable key, throwing an error if it is missing, which ensures proper configuration for authentication services.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-
+import { ClerkProvider } from '@clerk/clerk-react'
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
@@ -12,11 +12,19 @@ const store = configureStore({
   reducer: rootReducer,
 });
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+          <App />
+        </ClerkProvider>
       </BrowserRouter>
     </Provider>
   </StrictMode>
