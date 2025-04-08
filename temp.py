5bed663b613c713886b990ba1b65f3525af05ba4
Mihir <mihir.pande22@pccoepune.org>
2025-04-08 16:29:20 +0530

    refactor(database): use environment variable for PostgreSQL connection string

--- temp.py
+++ temp.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine, text
+import os
 
 # PostgreSQL connection string
-NEON_CONNECTION_STRING = "postgresql://u9jp4ii7me8i14:pfa0c4247d74009ded7923ad12f768fb8838910ddaf06bcce9a8ddfb36c35a605@c1i13pt05ja4ag.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/df9ksv4fo25hui"
+NEON_CONNECTION_STRING = os.getenv("DATABASE_URL")
 
 # Create engine
 engine = create_engine(NEON_CONNECTION_STRING)
